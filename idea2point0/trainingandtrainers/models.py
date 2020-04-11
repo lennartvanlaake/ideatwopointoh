cdd39258d0152c2c33e60b091625f0b27be22e7a
b.merckens <bionda.merckens@isatis-health.nl>
2020-04-11 18:42:12 +0200

    nieuwe versie

--- idea2point0/trainingandtrainers/models.py
+++ idea2point0/trainingandtrainers/models.py
@@ -8,6 +8,20 @@ from wagtail.contrib.routable_page.models import RoutablePageMixin, route
 from wagtail.search import index
 from django.core.validators import FileExtensionValidator
 
+# permission pages
+class TrainerPermission(Page):
+
+    subpage_types = ['TrainingContent','TrainingEvent', 'Trainer' ]
+    
+
+class MasterTrainerPermission(Page):
+
+    subpage_types = ['TrainingContent', 'TTTEvent']
+
+class SteeringComPermission(Page):
+
+    subpage_types = ['AllContent', 'TTTEvent','BlankNewPage']
+
 class IdeaTrainingIndex(RoutablePageMixin, Page):
 
     @route(r'^search/$')
@@ -56,17 +70,47 @@ CATEGORY_CHOICES = [
     ('Pedagogy',    'Pedagogy Content'),
 ]
 
-class IdeaTrainingIndex(Page):
-    intro = RichTextField(blank=True)
+TRAININGEVENT_CHOICES = [
+    ('Training',    'Training'),
+    ('TTT',         'Train-the-Trainer')
+]
 
-    content_panels = Page.content_panels + [
-        FieldPanel('intro', classname="full")
+
+
+
+class TrainingContent(Page):
+    parent_page_types = ['TrainerPermission','MasterTrainerPermission']
+
+    date = models.DateField("Training created", auto_now = True)
+    subject = models.CharField(max_length=100)
+    targetAudience = models.CharField(choices=YEAR_IN_SCHOOL_CHOICES, max_length=250)
+    level =  models.CharField(choices=LEVEL_CHOICES, max_length=250)
+    category = models.CharField(default='Training', max_length=250)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=250)
+    summary = models.CharField(max_length=3000)
+    training = models.FileField(upload_to='trainings/',
+                                validators=[FileExtensionValidator(allowed_extensions=['pdf'])])
+    
+    search_fields = Page.search_fields + [
+        index.SearchField('subject'),
+        index.SearchField('targetAudience'),
+        index.SearchField('level'),
+        index.SearchField('category'),
+        index.SearchField('language')
     ]
 
+    content_panels = Page.content_panels + [
+        FieldPanel('subject'),
+        FieldPanel('targetAudience'),
+        FieldPanel('level'),
+        FieldPanel('language'),
+        FieldPanel('summary'),
+        FieldPanel('training')
+    ]
 
+class AllContent(Page):
+    parent_page_types = ['SteeringComPermission']
 
-
-class IdeaTraining(Page):
     date = models.DateField("Training created", auto_now = True)
     subject = models.CharField(max_length=100)
     targetAudience = models.CharField(choices=YEAR_IN_SCHOOL_CHOICES, max_length=250)
@@ -76,20 +120,16 @@ class IdeaTraining(Page):
     summary = models.CharField(max_length=3000)
     training = models.FileField(upload_to='trainings/',
                                 validators=[FileExtensionValidator(allowed_extensions=['pdf'])])
-
-
+    
     search_fields = Page.search_fields + [
         index.SearchField('subject'),
         index.SearchField('targetAudience'),
         index.SearchField('level'),
         index.SearchField('category'),
         index.SearchField('language')
-
-
     ]
 
     content_panels = Page.content_panels + [
-        #FieldPanel('date'),
         FieldPanel('subject'),
         FieldPanel('targetAudience'),
         FieldPanel('level'),
@@ -102,19 +142,26 @@ class IdeaTraining(Page):
 
 
 class Trainer(Page):
+    parent_page_types = ['TrainerPermission']
+
     profilePicture = models.ImageField(upload_to='trainers/',
                                 validators=[FileExtensionValidator(allowed_extensions=['jpg','png,'])])
-    #date = models.DateField("Training created", auto_now = True)
     name = models.CharField(max_length=100)
     age = models.IntegerField()
     city =  models.CharField(max_length=100)
     country = models.CharField(max_length=100)
     #languagesSpoken = models.MultiSelectField(choices=LANGUAGE_CHOICES, max_length=250)
+    languagesSpoken1 = models.CharField(choices=LANGUAGE_CHOICES, max_length=250)
+    languagesSpoken2 = models.CharField(choices=LANGUAGE_CHOICES, max_length=250, blank=True)
+    languagesSpoken3 = models.CharField(choices=LANGUAGE_CHOICES, max_length=250, blank=True)
     shortBio = models.CharField(max_length=3000)
 
     search_fields = Page.search_fields + [
         index.SearchField('name'),
-        index.SearchField('country')
+        index.SearchField('country'),
+        index.SearchField('languagesSpoken1'),
+        index.SearchField('languagesSpoken2'),
+        index.SearchField('languagesSpoken3')     
     ]
 
     content_panels = Page.content_panels + [
@@ -126,8 +173,11 @@ class Trainer(Page):
         FieldPanel('shortBio'),
     ]
 
+ 
 
-class TrainingEvent(Page):
+class TTTEvent(Page):
+    parent_page_types = ['SteeringComPermission','MasterTrainerPermission']
+
     Title = models.CharField(max_length=100)
     date = models.DateTimeField("date and time of event")
     street = models.CharField("Street and number",max_length=100)
@@ -139,6 +189,7 @@ class TrainingEvent(Page):
     trainer1 = models.CharField(max_length=100)
     trainer2 = models.CharField(max_length=100,blank=True)
     trainer3 = models.CharField(max_length=100,blank=True)
+    typetraining = models.CharField("type of training",max_length=100,blank=True,choices=TRAININGEVENT_CHOICES )
 
     search_fields = Page.search_fields + [
         index.SearchField('title'),
@@ -147,7 +198,50 @@ class TrainingEvent(Page):
         index.SearchField('country'),
         index.SearchField('trainer1'),
         index.SearchField('trainer2'),
-        index.SearchField('trainer3')
+        index.SearchField('trainer3'),
+        index.SearchField('typetraining')
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('title'),
+        FieldPanel('date'),
+        FieldPanel('street'),
+        FieldPanel('city'),
+        FieldPanel('country'),
+        FieldPanel('contactemail'),
+        FieldPanel('contactwebsite'),
+        FieldPanel('description'),
+        FieldPanel('trainer1'),
+        FieldPanel('trainer2'),
+        FieldPanel('trainer3'),
+        FieldPanel('typetraining')
+    ]
+
+class TrainingEvent(Page):
+    parent_page_types = ['TrainerPermission']
+
+    Title = models.CharField(max_length=100)
+    date = models.DateTimeField("date and time of event")
+    street = models.CharField("Street and number",max_length=100)
+    city =  models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    contactemail = models.CharField("Contact Emailadresss",max_length=100,blank=True)
+    contactwebsite = models.CharField("Contact website",max_length=100,blank=True)
+    description = models.CharField(max_length=3000)
+    trainer1 = models.CharField(max_length=100)
+    trainer2 = models.CharField(max_length=100,blank=True)
+    trainer3 = models.CharField(max_length=100,blank=True)
+    typetraining = models.CharField("type of training",max_length=100,blank=True,default='Training' )
+
+    search_fields = Page.search_fields + [
+        index.SearchField('title'),
+        index.SearchField('date'),
+        index.SearchField('city'),
+        index.SearchField('country'),
+        index.SearchField('trainer1'),
+        index.SearchField('trainer2'),
+        index.SearchField('trainer3'),
+        index.SearchField('typetraining')
     ]
 
     content_panels = Page.content_panels + [
@@ -162,8 +256,15 @@ class TrainingEvent(Page):
         FieldPanel('trainer1'),
         FieldPanel('trainer2'),
         FieldPanel('trainer3')
-
     ]
 
+class BlankNewPage(Page):
+    parent_page_types = ['SteeringComPermission']
 
-    
+    Title = RichTextField(blank=True)
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('title'),
+        FieldPanel('body')
+    ]
