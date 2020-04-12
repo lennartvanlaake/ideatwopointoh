1aa023c21a56f44d724e3b0234dfc6f05f3a70cb
Your Name <you@example.com>
2020-04-12 18:28:19 +0200

    attempt to fix prod settings

--- idea2point0/idea2point0/settings/prod.py
+++ idea2point0/idea2point0/settings/prod.py
@@ -1,9 +1,14 @@
 from .base import *
+try:
+    from .local import *
+except ImportError:
+    pass
 
 DEBUG = False
 
 SECRET_KEY = os.environ["SECRET_KEY"]
 password = os.environ['POSTGRES_PASSWORD']
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -14,5 +19,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+
 
 
